385a6f3592628a5c1a97350b61a7f0dab017dd1f
Ben Thornton <ben.thornton@hexagon.com>
2024-06-26 09:04:19 -0700

    added warning about importing the threading module

--- .vscode-test.js
+++ .vscode-test.js
@@ -3,26 +3,16 @@ const { defineConfig } = require("@vscode/test-cli");
 const { getLatestAdamsVersions } = require("./test/utils.js");
 const path = require("path");
 
-const adamsVersionDir = path.dirname(path.dirname(path.dirname(process.env.ADAMS_LAUNCH_COMMAND)));
-const adamsLaunchCommands = [
-    path.join(adamsVersionDir, "2023_2", "common", "mdi.bat"),
-    path.join(adamsVersionDir, "2022_4_904865", "common", "mdi.bat"),
-];
-module.exports = defineConfig(
-    adamsLaunchCommands.map((adamsLaunchCommand) => {
-        return {
-            files: "test/**/*.test.js",
-            workspaceFolder: "test",
-            env: {
-                _ADAMS_LAUNCH_COMMAND: adamsLaunchCommand,
-            },
-            label: path.basename(path.dirname(path.dirname(adamsLaunchCommand))),
-            mocha: {
-                timeout: 300000,
-                require: path.join(__dirname, "test", "global_fixture.cjs"),
-            },
-        };
-    })
-);
-
-// BUG: Tries to run both versions of Adams in parallel
+const adamsLaunchCommand = getLatestAdamsVersions();
+module.exports = defineConfig({
+    files: "test/**/*.test.js",
+    workspaceFolder: "test",
+    env: {
+        _ADAMS_LAUNCH_COMMAND: adamsLaunchCommand,
+    },
+    label: path.basename(path.dirname(path.dirname(adamsLaunchCommand))),
+    mocha: {
+        timeout: 300000,
+        require: path.join(__dirname, "test", "global_fixture.cjs"),
+    },
+});
